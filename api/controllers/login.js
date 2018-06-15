cb75efd4d676ac42d200e75ebb16bdb594e0a397
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-06-15 14:19:56 -0400

    Completed MVP.

--- api/controllers/login.js
+++ api/controllers/login.js
@@ -15,6 +15,7 @@ const login = (req, res) => {
     }
     user.checkPassword(password, (nonMatch, hashMatch) => {
       // This is an example of using our User.method from our model.
+      console.log('No match', nonMatch);
       if (nonMatch !== null) {
         res.status(422).json({ error: 'passwords dont match' });
         return;
--- api/controllers/user.js
+++ api/controllers/user.js
@@ -5,6 +5,14 @@ const createUser = (req, res) => {
   const { username, password } = req.body;
   // create user takes in the username and password and saves a user.
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
+
+  User.create({ username, password })
+    .then(user => {
+      res.status(201).json({ username: user.username });
+    })
+    .catch(error => {
+      res.status(500).json(error.message);
+    })
 };
 
 module.exports = {
